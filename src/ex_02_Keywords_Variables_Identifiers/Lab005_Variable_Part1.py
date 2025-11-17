53d2dfb7a206751867f8996de5b25885beaf95c6
nimisha <ndesai2mail@gmail.com>
2025-11-17 17:09:12 +0530

    9th October, 2025 - updated folder structure

---
+++ src/ex_02_Keywords_Variables_Identifiers/Lab005_Variable_Part1.py
@@ -0,0 +1,8 @@
+# Python is dynamic typed language.
+from operator import truediv
+
+age = 65
+age = "nimisha"
+age = 234.45
+
+# we can create duplicate variables. python takes latest value.
---
+++ src/ex_02_Keywords_Variables_Identifiers/Lab006_Identifier.py
@@ -0,0 +1,10 @@
+# Variable name is the identifier.
+_ = 12
+print(_)
+_ = _ + 1
+print(_)
+
+# 1. Underscore can be variable name but number as a first character in variable name is not possible.
+# Variable name can be alpha numeric or identifier.
+a43 = 43
+print(a43)
